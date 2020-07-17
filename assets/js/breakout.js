b8333ba67f6c27b5aa27a2efe4142cf4790441d8
fwauters <fwauters13@gmail.com>
2020-07-17 16:31:37 +0200

    add start msg

--- assets/js/breakout.js
+++ assets/js/breakout.js
@@ -120,6 +120,45 @@ function collisionDetection() {
     }
 }
 
+function startMessage() {
+    // Define font
+    ctx.font = "16px Arial";
+    // Define font color
+    ctx.fillStyle = "white";
+    // Define and position text(text, xPos, yPos)
+    ctx.fillText("Welcome in Breakout 2D \ntest test test", 8, 20);
+}
+
+//https://stackoverflow.com/questions/5026961/html5-canvas-ctx-filltext-wont-do-line-breaks
+//http://jsfiddle.net/BaG4J/5/
+function printAtWordWrap(context, text, x, y, lineHeight, fitWidth) {
+    fitWidth = fitWidth || 0;
+
+    if (fitWidth <= 0) {
+        context.fillText(text, x, y);
+        return;
+    }
+    var words = text.split(' ');
+    var currentLine = 0;
+    var idx = 1;
+    while (words.length > 0 && idx <= words.length) {
+        var str = words.slice(0, idx).join(' ');
+        var w = context.measureText(str).width;
+        if (w > fitWidth) {
+            if (idx == 1) {
+                idx = 2;
+            }
+            context.fillText(words.slice(0, idx - 1).join(' '), x, y + (lineHeight * currentLine));
+            currentLine++;
+            words = words.splice(idx - 1);
+            idx = 1;
+        }
+        else { idx++; }
+    }
+    if (idx > 0)
+        context.fillText(words.join(' '), x, y + (lineHeight * currentLine));
+}
+
 function drawScore() {
     // Define font
     ctx.font = "16px Arial";
@@ -230,6 +269,12 @@ function draw() {
 }
 
 //------------------------------------- Execution --------------------------------------------------------------------------------
+
+startMessage();
+
+//let msg = "ceci est un très long messge qui me sert de test !";
+//printAtWordWrap(ctx, msg, 10, 10, 5, 5);
+
 document.getElementById("btn").addEventListener("click", () => {
     // Use draw function every 10ms
     interval = setInterval(draw, 10);
